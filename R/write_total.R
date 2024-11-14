4623e4c65b44dd77f42c22e13afc0a52060a9bbb
Holley <Colt.Holley@dfw.wa.gov>
2024-11-14 15:41:40 -0800

    split write_db_tables into component functions

---
+++ R/write_total.R
@@ -0,0 +1,9 @@
+write_total <- function(con, creel_estimates_db) {
+  DBI::dbWriteTable(
+    conn = con,
+    name = DBI::Id(schema = "creel", table = "model_estimates_total"),
+    value = creel_estimates_db$total,
+    row.names = FALSE,
+    overwrite = FALSE,
+    append = TRUE)
+}
